8a8fb763242640ac310e9ddd074a6d09684e7b64
Hiroki.T <hirodora@me.com>
2021-12-30 16:25:20 +0900

    add: IERC777Recipient

--- contract/contracts/HardcoreBank.sol
+++ contract/contracts/HardcoreBank.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.8.0;
 
 import "OpenZeppelin/openzeppelin-contracts@4.4.1/contracts/utils/math/SafeMath.sol";
+import "OpenZeppelin/openzeppelin-contracts@4.4.1/contracts/token/ERC777/ERC777.sol";
 
 
 struct Config {
@@ -13,7 +14,7 @@ struct Config {
     uint256 monthlyRemittrance;
 }
 
-contract HardcoreBank {
+contract HardcoreBank is IERC777Recipient {
     using SafeMath for uint256;
 
     uint256 constant decimal = 18;
@@ -22,6 +23,10 @@ contract HardcoreBank {
 
     constructor(string memory subject, string memory description, address token, uint256 targetAmount, uint256 monthlyRemittrance) {
         config = Config(msg.sender, subject, description, token, targetAmount, monthlyRemittrance);
+    }
+
+    function tokensReceived(address operator, address from, address to, uint256 amount, bytes calldata userData, bytes calldata operatorData) external {
+        // TODO: write receiver
     }
 }
 
