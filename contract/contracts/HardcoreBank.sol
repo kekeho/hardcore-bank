0c872c3235dac375e1abb5471f6b9fc6f9d15f37
Hiroki.T <hirodora@me.com>
2021-12-31 17:25:08 +0900

    add: disable account

--- contract/contracts/HardcoreBank.sol
+++ contract/contracts/HardcoreBank.sol
@@ -11,6 +11,8 @@ struct Config {
     address tokenContractAddress;  // token address
     uint256 targetAmount;
     uint256 monthlyRemittrance;
+
+    bool disabled;
 }
 
 contract HardcoreBank is IERC777Recipient {
@@ -22,6 +24,7 @@ contract HardcoreBank is IERC777Recipient {
     IERC1820Registry private _erc1820Registry = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
 
     mapping(address => Config[]) private accountList;
+    mapping(address => uint256) public activeCount;
 
     constructor() {
         // set owner;
@@ -33,12 +36,32 @@ contract HardcoreBank is IERC777Recipient {
 
     function createAccount(string calldata subject, string calldata description, address token, uint256 targetAmount, uint256 monthlyRemittrance) public {
         accountList[msg.sender].push(
-            Config(msg.sender, subject, description, token, targetAmount, monthlyRemittrance)
+            Config(msg.sender, subject, description, token, targetAmount, monthlyRemittrance, false)
         );
+        activeCount[msg.sender] = activeCount[msg.sender].add(1);
     }
 
-    function getAccount() public view returns (Config[] memory) {
-        return accountList[msg.sender];
+    function getAccounts() public view returns (Config[] memory) {
+        Config[] memory result = new Config[](activeCount[msg.sender]);
+
+        uint256 result_i = 0;
+        for (uint256 i = 0; i < accountList[msg.sender].length; i=i.add(1)) {
+            // pass disabled
+            if (accountList[msg.sender][i].disabled == false) {
+                result[result_i] = accountList[msg.sender][i];
+                result_i = result_i.add(1);
+            }
+        }
+
+        return result;
+    }
+
+    function disable(uint256 id) public {
+        require(id < accountList[msg.sender].length);
+        require(accountList[msg.sender][id].disabled == false);
+
+        accountList[msg.sender][id].disabled = true;
+        activeCount[msg.sender] = activeCount[msg.sender].sub(1);
     }
 
     function isOwner() public view returns (bool) {
--- contract/tests/test_account.py
+++ contract/tests/test_account.py
@@ -12,7 +12,7 @@ def test_createAccount_getAccount(deploy_erc1820_register):
     monthly = 1e5
 
     c.createAccount(name, description, token, total_amount, monthly, {'from': accounts[1]})
-    config_list = c.getAccount({'from': accounts[1]})
+    config_list = c.getAccounts({'from': accounts[1]})
 
     assert len(config_list) == 1
 
@@ -27,7 +27,7 @@ def test_createAccount_getAccount(deploy_erc1820_register):
     assert config_head[5] == monthly
 
     # other account
-    assert len(c.getAccount({'from': accounts[0]})) == 0
+    assert len(c.getAccounts({'from': accounts[0]})) == 0
 
 
 def test_createAccount_getAccount_many(deploy_erc1820_register):
@@ -46,7 +46,7 @@ def test_createAccount_getAccount_many(deploy_erc1820_register):
 
     for i, (n, d, t, a, m) in enumerate(zip(name, description, token, total_amount, monthly)):
         c.createAccount(n, d, t, a, m, {'from': accounts[0]})
-        accounts_list = c.getAccount({'from': accounts[0]})
+        accounts_list = c.getAccounts({'from': accounts[0]})
 
         assert len(accounts_list) == i+1
 
@@ -57,3 +57,32 @@ def test_createAccount_getAccount_many(deploy_erc1820_register):
         assert config_head[3] == token[i]
         assert config_head[4] == total_amount[i]
         assert config_head[5] == monthly[i]
+
+
+def test_disable(deploy_erc1820_register):
+    c = HardcoreBank.deploy({'from': accounts[0]})
+
+    # many account
+    name = ['hoge','fuga', 'piyo']
+    description = ['hogefuga', 'fugapiyo', 'piyohoge']
+    token = [
+        '0x2AC170958D2ee523a225620A994597C1AD831ec9',
+        '0x2AC07095892ee523a225620A994797C1AD831ec2',
+        '0x2AC170958D2ee523a225600A994597C1AD831ec8',
+    ]
+    total_amount = [1e19, 1e17, 9e18]
+    monthly = [1e6, 1e4, 1e10]
+
+    # create accounts
+    for (n, d, t, a, m) in zip(name, description, token, total_amount, monthly):
+        c.createAccount(n, d, t, a, m, {'from': accounts[0]})
+    accounts_list = c.getAccounts({'from': accounts[0]})
+    assert len(accounts_list) == 3
+
+    # disable
+    c.disable(1, {'from': accounts[0]})
+
+    # get accounts
+    accounts_list = c.getAccounts({'from': accounts[0]})
+    assert len(accounts_list) == 2
+    assert name[1] not in [a[1] for a in accounts_list]
