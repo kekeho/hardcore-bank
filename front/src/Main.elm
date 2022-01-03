ffcaf61ae0cd854b48c3eb2ad78a6b307d0ff4c0
Hiroki.T <hirodora@me.com>
2022-01-03 15:59:28 +0900

    add: form

--- front/src/Main.elm
+++ front/src/Main.elm
@@ -6,10 +6,11 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Url
 import Url.Parser
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, onInput)
 import Browser exposing (Document)
 
 import Model exposing (..)
+
 
 
 main : Program () Model Msg
@@ -26,12 +27,22 @@ main =
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init () url key =
-    ( Model key url , Cmd.none )
+    ( Model key url Nothing Model.initAddfield , Cmd.none )
 
 
 type Msg
     = UrlRequested Browser.UrlRequest
     | UrlChanged Url.Url
+    | AddField AddFieldMsg
+
+
+type AddFieldMsg
+    = Subject String
+    | Description String
+    | ContractAddress String
+    | TargetAmount String
+    | MonthlyRemittrance String
+    | Submit
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -47,6 +58,43 @@ update msg model =
 
         UrlChanged url ->
             ( { model | url = url }
+            , Cmd.none
+            )
+        
+        AddField subMsg ->
+            let
+                addField = model.addField
+                addField_ = case subMsg of
+                    Subject subject ->
+                        { addField | subject = subject }
+                    Description desc ->
+                        { addField | description = desc }
+                    ContractAddress ca ->
+                        { addField | contractAddress = ca }
+                    TargetAmount maybeTa ->
+                        case String.toFloat maybeTa of
+                            Just ta ->
+                                { addField | targetAmount = ta } 
+                            Nothing ->
+                                addField
+                    MonthlyRemittrance maybeMr ->
+                        case String.toFloat maybeMr of
+                            Just mr ->
+                                { addField | monthlyRemittrance = mr } 
+                            Nothing ->
+                                addField
+                    Submit ->
+                        -- validate
+                        let
+                            errors = addFormValidate addField
+                        in
+                        if List.length errors == 0 then
+                            { addField | errors = [] }
+                        else
+                            { addField | errors = errors } 
+
+            in
+            ( { model | addField = addField_ }
             , Cmd.none
             )
 
@@ -67,7 +115,7 @@ view model =
             Just Index ->
                 accountListView
             Just Add ->
-                addView
+                addView model
             Nothing ->
                 notFoundView
     in
@@ -122,9 +170,83 @@ accountListView =
     }
 
 
-addView : Document Msg
-addView =
+addView : Model -> Document Msg
+addView model =
     { title = "Create Account"
     , body =
-        []
+        [ div [ class "add" ]
+            [ div [ class "row" ]
+                [ div [ class "row-title" ]
+                    [ h1 []
+                        [ text "Create Account" ]
+                    ]
+                , div [ class "form" ]
+                    [ label [ for "subject" ] [ text "Subject" ]
+                    , input
+                        [ id "subject", type_ "text"
+                        , value model.addField.subject
+                        , onInput <| (\x -> AddField <| Subject x)
+                        ]
+                        []
+                    , case List.filter (\(f, _) -> f == Model.SubjectField) model.addField.errors of
+                        [] ->
+                            p [] []
+                        (_, e) :: _ ->
+                            p [ class "error" ] [ text e ]
+                    , label [ for "description" ] [ text "Description" ]
+                    , input
+                        [ id "description", type_ "text"
+                        , value model.addField.description
+                        , onInput <| (\x -> AddField <| Description x)
+                        ]
+                        []
+                    , case List.filter (\(f, _) -> f == Model.DescriptionField) model.addField.errors of
+                        [] ->
+                            p [] []
+                        (_, e) :: _ ->
+                            p [ class "error" ] [ text e ]
+                    , label [ for "contract-address" ] [ text "Token Contract Address" ]
+                    , input
+                        [ id "contract-address", type_ "text"
+                        , value model.addField.contractAddress
+                        , onInput <| (\x -> AddField <| ContractAddress x)
+                        ]
+                        []
+                    , case List.filter (\(f, _) -> f == Model.ContractAddressField) model.addField.errors of
+                        [] ->
+                            p [] []
+                        (_, e) :: _ ->
+                            p [ class "error" ] [ text e ]
+                    , label [ for "target-amount", class "small" ] [ text "Target Amount" ]
+                    , input
+                        [ id "target-amount", type_ "number"
+                        , value <| String.fromFloat model.addField.targetAmount
+                        , step "0.00001"
+                        , onInput <| (\x -> AddField <| TargetAmount x)
+                        ]
+                        []
+                    , case List.filter (\(f, _) -> f == Model.TargetAmountField) model.addField.errors of
+                        [] ->
+                            p [] []
+                        (_, e) :: _ ->
+                            p [ class "error" ] [ text e ]
+                    , label [ for "monthly-remittrance", class "small" ] [ text "Monthly Remittrance" ]
+                    , input
+                        [ id "monthly-remittrance", type_ "number"
+                        , value <| String.fromFloat model.addField.monthlyRemittrance
+                        , step "0.00001"
+                        , onInput <| (\x -> AddField <| MonthlyRemittrance x)
+                        ]
+                        []
+                    , case List.filter (\(f, _) -> f == Model.MonthlyRemittranceField) model.addField.errors of
+                        [] ->
+                            p [] []
+                        (_, e) :: _ ->
+                            p [ class "error" ] [ text e ]
+                    , button [ class "submit", onClick <| AddField Submit ]
+                        [ text "Create" ]
+                    ]
+                ]
+            ]
+        ]
     }
--- front/src/Model.elm
+++ front/src/Model.elm
@@ -4,12 +4,120 @@ import Browser.Navigation as Nav
 import Url
 import Url.Parser
 import Html exposing (a)
+import Html exposing (address)
 
 
 type alias Model =
     { key : Nav.Key
     , url : Url.Url
+    , address : Maybe Address
+    , addField : AddField
     }
+
+
+type alias Address = String
+
+
+type AddFieldType
+    = SubjectField
+    | DescriptionField
+    | ContractAddressField
+    | TargetAmountField
+    | MonthlyRemittranceField
+
+type alias AddField =
+    { subject : String
+    , description : String
+    , contractAddress : Address
+    , targetAmount : Float
+    , monthlyRemittrance : Float
+    , errors : List (AddFieldType, String)
+    }
+
+
+initAddfield : AddField
+initAddfield =
+    AddField
+        ""
+        ""
+        ""
+        0.0
+        0.0
+        []
+
+
+-- Func
+
+addFormValidate : AddField -> List (AddFieldType, String)
+addFormValidate field =
+    List.filterMap 
+        (\x -> x)
+        [ validateStringLength SubjectField field.subject
+        , validateStringLength DescriptionField field.description
+        , validateContractAddress field.contractAddress
+            |> (\(b, s) -> if b then Nothing else Just (ContractAddressField, s))
+        , validateMin TargetAmountField 0.0 field.targetAmount
+        , validateMin MonthlyRemittranceField 0.0 field.monthlyRemittrance
+        ]
+
+
+validateMin : AddFieldType -> Float -> Float -> Maybe (AddFieldType, String)
+validateMin field min val =
+    if val > min then
+        Nothing
+    else
+        Just (field, "Value must be greater than " ++ String.fromFloat min)
+
+
+validateStringLength : AddFieldType -> String -> Maybe (AddFieldType, String)
+validateStringLength field str =
+    if String.length str == 0 then
+        Just (field, "The length of the string must be greater than or equal to 1.")
+    else
+        Nothing
+
+
+validateContractAddress : Address -> (Bool, String)
+validateContractAddress address =
+    if String.length address /= 42 then
+        (False, "The contract address must be 42 characters long.")
+    else
+        case String.slice 0 2 address of
+            "0x" ->
+                if onlyHex <| String.slice 2 (String.length address) address then                   
+                    (True, "")
+                else
+                    (False, "The contract address should be in hexadecimal notation.")
+            _ ->
+                (False, "The beginning of the contract address should be \"0x\".")
+        
+
+isJust : Maybe a -> Bool
+isJust maybe =
+    case maybe of
+        Just x ->
+            True
+        Nothing ->
+            False
+
+
+onlyHex : String -> Bool
+onlyHex str =
+    if String.length str == 0 then
+        True
+    else
+        let 
+            head =
+                String.slice 0 1 str
+                    |> String.toLower
+        in
+        if String.contains head "0123456789abcdef" then
+            onlyHex <| String.slice 1 (String.length str) str
+        else
+            False
+           
+
+
 
 
 -- Route
